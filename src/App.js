ac427e8d99c85a229e7b6dcdfa75b6c971165a98
Jerry Zhou <81694589+je-zhou@users.noreply.github.com>
2024-04-23 11:50:04 +1000

    Activating app routing + adding links to Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
-// import AboutPage from "./pages/AboutPage";
-// import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/blog" element={<ContactPage />} />
         </Routes>
-      </BrowserRouter> */}
-      <NavBar />
-      <HomePage />
+      </BrowserRouter>
+      {/* <NavBar />
+      <HomePage /> */}
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="bg-yellow-400 p-4 border-b-2 border-gray-800">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-lg font-semibold">
-          <button className="hover:text-white p-2 rounded-lg">
-            My Portfolio
-          </button>
+          <Link to="/" className="hover:text-white p-2 rounded-lg">My Portfolio</Link>
         </div>
         <div>
-          <button className="hover:text-white px-3">Experience</button>
-          <button className="hover:text-white px-3">Contact</button>
+          <Link to="/blog" className="hover:text-white px-3">Blog</Link>
+          <Link to="/experience" className="hover:text-white px-3">Experience</Link>
+          <Link to="/contact" className="hover:text-white px-3">Contact</Link>
         </div>
       </div>
     </nav>

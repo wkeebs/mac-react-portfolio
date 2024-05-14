7a33122803e73912dc5a9f4ba60d41572cfe1d5e
wkeebs <will.a.keeble@gmail.com>
2024-05-15 09:52:38 +1000

    Add useEffect content

--- src/pages/BlogPostPage.jsx
+++ src/pages/BlogPostPage.jsx
@@ -1,41 +1,68 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { blogPosts } from '../blogsPosts/blogPosts'
-import { useState } from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { blogPosts } from "../blogsPosts/blogPosts";
+import { useState, useEffect } from "react";
+
+const USER_URL = "https://jsonplaceholder.typicode.com/users";
 
 export default function BlogPost() {
-  const { slug } = useParams()
-  const { title, imgUrl, content } = blogPosts[slug];
+    const { slug } = useParams();
+    const { title, imgUrl, content } = blogPosts[slug];
 
-  // React Hooks - Use State
+    // React Hooks - Use State
 
-  const [upvotes, setUpvotes] = useState(0)
+    const [upvotes, setUpvotes] = useState(0);
 
-  function onUpvote() {
-    setUpvotes(upvotes + 1)
-  }
-
-  function onDownvote() {
-    if (upvotes > 0) {
-      setUpvotes(upvotes - 1)
+    function onUpvote() {
+        setUpvotes(upvotes + 1);
     }
-  }
 
-  return (
-    <div className='flex flex-col items-center space-y-4 p-10'>
-      <img className='h-96 w-96 rounded-full object-cover' src={imgUrl} alt={imgUrl} />
-      <h1 className='font-bold text-4xl'>{title}</h1>
-      <p className='text-lg'>{content}</p>
-      {/* Upvoting */}
-      <div className='flex p-2 items-center justify-center space-x-2'>
-        <button className='border p-2 rounded-xl px-3 hover:bg-slate-100 transition' onClick={onUpvote}>
-          👍
-        </button>
-        <p>{upvotes}</p>
-        <button className='border p-2 rounded-xl px-3 hover:bg-slate-100 transition' onClick={onDownvote}>
-          👎
-        </button>
-      </div>
-    </div>
-  )
+    function onDownvote() {
+        if (upvotes > 0) {
+            setUpvotes(upvotes - 1);
+        }
+    }
+
+    // Use Effect - Set username
+    const [username, setUsername] = useState("Loading...");
+
+    useEffect(() => {
+        fetch(USER_URL)
+            .then((res) => res.json())
+            .then((users) => setUsername(users[0].username));
+    }, []);
+
+    // Use Effect - Other side effects
+    useEffect(() => {
+      console.log("Upvotes: " + upvotes)
+    }, [upvotes])
+
+    return (
+        <div className="flex flex-col items-center space-y-4 p-10">
+            <img
+                className="h-96 w-96 rounded-full object-cover"
+                src={imgUrl}
+                alt={imgUrl}
+            />
+            <h1 className="font-bold text-4xl">{title}</h1>
+            <h2>by {username}</h2>
+            <p className="text-lg">{content}</p>
+            {/* Upvoting */}
+            <div className="flex p-2 items-center justify-center space-x-2">
+                <button
+                    className="border p-2 rounded-xl px-3 hover:bg-slate-100 transition"
+                    onClick={onUpvote}
+                >
+                    👍
+                </button>
+                <p>{upvotes}</p>
+                <button
+                    className="border p-2 rounded-xl px-3 hover:bg-slate-100 transition"
+                    onClick={onDownvote}
+                >
+                    👎
+                </button>
+            </div>
+        </div>
+    );
 }
